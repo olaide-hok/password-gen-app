ded36de2c8e79eaff61921ca33c5a590eba078c2
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-02-14 16:49:22 +0100

    add strength indicator functionality

--- index.js
+++ index.js
@@ -11,11 +11,71 @@ const includeUppercase = document.getElementById("uppercase");
 const includeNumbers = document.getElementById("numbers");
 const includeSymbols = document.getElementById("symbols");
 
+// Select the strength indicator element
+const strengthLevel = document.getElementById("strengthLevel");
+const strengthLevelIconsWrapper = document.querySelector(".icons");
+
 // Character sets
 const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
 const numbers = "0123456789";
 const symbols = "!@#$%^&*()";
+
+// Function to calculate password strength
+const calculateStrength = () => {
+  let inclusionCount = 0;
+  if (includeUppercase.checked) inclusionCount++;
+  if (includeLowercase.checked) inclusionCount++;
+  if (includeNumbers.checked) inclusionCount++;
+  if (includeSymbols.checked) inclusionCount++;
+
+  // Determine strength level based on inclusion count
+  if (inclusionCount === 0) {
+    strengthLevel.classList.remove("d-none");
+    strengthLevel.textContent = "Too Weak";
+  } else if (inclusionCount === 1) {
+    strengthLevel.classList.remove("d-none");
+    strengthLevel.textContent = "Too Weak";
+  } else if (inclusionCount === 2) {
+    strengthLevel.textContent = "Weak";
+    strengthLevel.classList.remove("d-none");
+  } else if (inclusionCount === 3) {
+    strengthLevel.textContent = "Medium";
+    strengthLevel.classList.remove("d-none");
+  } else if (inclusionCount === 4) {
+    strengthLevel.textContent = "Strong";
+    strengthLevel.classList.remove("d-none");
+  }
+
+  // Reset all strength indicators
+  strengthLevelIconsWrapper.querySelectorAll("span").forEach((bar) => {
+    bar.className = "rect"; // Reset to default class
+  });
+
+  // Update the strength indicator
+  for (let i = 0; i < inclusionCount; i++) {
+    if (inclusionCount === 1) {
+      strengthLevelIconsWrapper
+        .querySelectorAll("span")
+        [i].classList.add("too_weak");
+    }
+    if (inclusionCount === 2) {
+      strengthLevelIconsWrapper
+        .querySelectorAll("span")
+        [i].classList.add("weak");
+    }
+    if (inclusionCount === 3) {
+      strengthLevelIconsWrapper
+        .querySelectorAll("span")
+        [i].classList.add("medium");
+    }
+    if (inclusionCount === 4) {
+      strengthLevelIconsWrapper
+        .querySelectorAll("span")
+        [i].classList.add("strong");
+    }
+  }
+};
 
 // Function to generate a random string based on selected options
 const generateRandomString = (length) => {
@@ -50,6 +110,8 @@ pwdGenBtn.addEventListener("click", function () {
     passwordField.placeholder = "P4$5W0rD!";
     passwordField.style.opacity = 0.25;
   }
+
+  calculateStrength(); // Update strength indicator
 });
 
 clipboardIcon.addEventListener("click", function () {
